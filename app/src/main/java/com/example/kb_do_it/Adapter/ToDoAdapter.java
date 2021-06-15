0490d5046f7ea43a2bf3ae80d85733049be560af
Krishnao7o <krishna.bhola.1@gmail.com>
2021-06-15 19:32:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/kb_do_it/Adapter/ToDoAdapter.java
@@ -0,0 +1,61 @@
+package com.example.kb_do_it.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.kb_do_it.MainActivity;
+import com.example.kb_do_it.R;
+import com.example.kb_do_it.model.ToDoModel;
+
+import java.util.List;
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
+
+    private List<ToDoModel> todoList;
+    private MainActivity activity;
+
+    public ToDoAdapter(MainActivity activity) {
+        this.activity = activity;
+
+    }
+
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.task_layout, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        ToDoModel item = todoList.get(position);
+        holder.task.setText(item.getTask());
+        holder.task.setChecked(toBoolean(item.getStatus()));
+    }
+
+    public int getItemCount(){
+        return todoList.size();
+
+    }
+    private boolean toBoolean(int n){
+        return n!=0;
+    }
+
+    public void setTasks(List<ToDoModel> todoList){
+        this.todoList = todoList;
+        notifyDataSetChanged();
+    }
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        CheckBox task;
+
+        ViewHolder(View view){
+            super (view);
+            task = view.findViewById(R.id.ToDoCheckBox);
+        }
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/kb_do_it/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.kb_do_it;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.service.controls.templates.ToggleTemplate;
+
+import com.example.kb_do_it.Adapter.ToDoAdapter;
+import com.example.kb_do_it.model.ToDoModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView tasksRecyclerView;
+    private ToDoAdapter tasksAdapter;
+
+    private List<ToDoModel> taskList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        taskList = new ArrayList<>();
+
+        tasksRecyclerView = findViewById(R.id.taskRecyclerView);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        tasksAdapter = new ToDoAdapter(this);
+        tasksRecyclerView.setAdapter(tasksAdapter);
+
+        ToDoModel task = new ToDoModel();
+        task.setTask("This is a test");
+        task.setStatus(0);
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+        taskList.add(task);
+
+        tasksAdapter.setTasks(taskList);
+
+
+    }
+}
